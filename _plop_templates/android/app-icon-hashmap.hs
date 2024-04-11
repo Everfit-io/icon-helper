ee49464046bdacbcb4a81fe655e304917872f1d4
Khoa Huynh <khoahuynh@everfit.io>
2024-04-11 17:43:56 +0700

    [Khoa] Update app icon tools

--- _plop_templates/android/app-icon-hashmap.hs
+++ _plop_templates/android/app-icon-hashmap.hs
@@ -1,2 +1,2 @@
-"{{team}}" to LABEL.APP_ICON_BRANDING_{{team}},
+"{{team}}" to R.mipmap.ic_branding_launcher_{{team}},
         // CODE_GEN_APP_ICON_HASHMAP DONT REMOVE THIS
--- config.js
+++ config.js
@@ -1,8 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
-  projectUrl: "/Users/everfit-mac/StudioProjects/everfit-android"
+  projectUrl: "/Users/duckky/Documents/project/everfit/android/everfit-android"
 };
-=======
-  projectUrl: "/Users/lambui/AndroidStudioProjects/everfit-android"
-};
->>>>>>> 848a3eabd1e7d6fc56dd0b7087f9dbc2b181ec81
