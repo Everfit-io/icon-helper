d904ebf11a82ce248ab4cde49201641baf3d4ea3
everfit-mac <tudoan@everfit.io>
2025-05-07 14:21:38 +0700

    UP-54389, new app icons

--- _plop/android/app-icon-hashmap.js
+++ _plop/android/app-icon-hashmap.js
@@ -3,7 +3,7 @@ module.exports = {
   type: "modify",
   abortOnFail: false,
   path:
-    config.projectUrl + "/app/src/main/java/com/everfit/util/AppIconUtils.kt",
+    config.projectUrl + "/app/src/main/java/com/everfit/util/AppIcon1Utils.kt",
   templateFile: "_plop_templates/android/app-icon-hashmap.hs",
   pattern: "// CODE_GEN_APP_ICON_HASHMAP DONT REMOVE THIS"
 };
